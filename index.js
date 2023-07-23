99c8e62219f6cb3e43b66adc43e92e216ea0dc7a
Mr-Johny-Blaska <121683366+jan-blaska@users.noreply.github.com>
2023-07-23 18:44:54 +0200

    Database test Nb.3

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const appSettings = {
     databaseURL: "https://scrimba-solo-personalnotes-app-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
 const personalNotesInDB = ref(database, "personalNotes");
 
 const inputTextEl = document.getElementById("input-note");
 const inputTextAreaEl = document.getElementById("input-textarea");
 const saveNoteBtnEl = document.getElementById("save-note-btn");
+const notesContainerEl = document.getElementById("notes-container");
 
 saveNoteBtnEl.addEventListener("click", function() {
     console.log("button clicked");
@@ -19,11 +20,53 @@ saveNoteBtnEl.addEventListener("click", function() {
     
     push(personalNotesInDB, newNote);
 
-    clearInputElements()
+    clearInputElements();
 
+})
+
+onValue(personalNotesInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let notesArray = Object.entries(snapshot.val());
+
+        clearNotesContainerEl();
+
+        for (let i = 0; i < notesArray.length; i++) {
+            let currentNote = notesArray[i];
+            let currentNoteID = currentNote[0];
+            let currentNoteValue = currentNote[1];
+        }
+
+        appendNoteToNotesContainerEl(currentNoteID, currentNoteValue);
+
+    } else {
+        notesContainerEl.innerHTML = "No notes here... yet";
+    }
 })
 
 function clearInputElements() {
     inputTextEl.value = "";
     inputTextAreaEl.value = "";
 }
+
+function clearNotesContainerEl() {
+    notesContainerEl.innerHTML = "";
+}
+
+function appendNoteToNotesContainerEl(noteID, noteValue) {
+    
+    let newNoteDivEl = document.createElement("div");
+
+    let newNoteEl = document.createElement("h3");
+    let newDescriptionEl = document.createElement("p");
+    
+    newNoteEl.textContent = noteValue.note;
+    newDescriptionEl.textContent = noteValue.description;
+
+    newNoteDivEl.append(newNoteEl);
+    newNoteDivEl.append(newDescriptionEl);
+
+    newNoteDivEl.classList.add("note");
+
+    notesContainerEl.append(newNoteDivEl);
+}
+
